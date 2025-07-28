49cb5408b6fa2c58cc3b5ce0687fc794919bf603
BDmarket2024 <bdmarket.xyz@gmail.com>
2025-07-29 00:52:37 +0600

    Updated project with new features

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyARVVjjRT4tvlQZCoJrB1hb6J8cKmF2zUg",
+    authDomain: "bdproperty-d8a7b.firebaseapp.com",
+    projectId: "bdproperty-d8a7b",
+    storageBucket: "bdproperty-d8a7b.firebasestorage.app",
+    messagingSenderId: "503587327824",
+    appId: "1:503587327824:web:760c3b1e45688b85826655",
+    measurementId: "G-5PL2SR8N4E"
 };
 
 firebase?.initializeApp(firebaseConfig)
